1bccc8b22153a78e1b19d8eb0f4a0111b71c1e12
Lokman Hakim <53113647+lokcies@users.noreply.github.com>
2025-09-23 21:11:07 +0800

    create a new function called getComputerChoice
    use Math.random, and then use this generated number to decide which hand?

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+console.log("Hello World!");
+
+/* Create a new functikon named getComputerChoice
+- Randomly return one of the following string values: "rock", "papers", "scissors".
+*/
+function getComputerChoice() {
+    // toFixed function to round off numbers to 2 decimal places
+    let handNumbered = Math.random().toFixed(2);
+    console.log(handNumbered);
+}
+
+getComputerChoice();
